07130acdc4dc51710bf20036a85e93eeee63f983
Doan Diem <diemdt9403@gmail.com>
2023-12-13 21:55:34 +0700

    Responsive 9.55 13/12

---
+++ assets/JS/show.js
@@ -0,0 +1,12 @@
+
+var show = function (item , list, cnt) {
+  
+  item.addEventListener("click", function () {
+    cnt++;
+    if (cnt % 2 != 0) {
+      list.classList.add("show");
+    } else {
+      list.classList.remove("show");
+    }
+  });
+};
