d9604d0cdfeb0539aa0f67cffb2255b1ecda27d9
Doan Diem <diemdt9403@gmail.com>
2023-12-21 22:33:41 +0700

    cap nhat tai khoan user/admin

--- assets/JS/show.js
+++ assets/JS/show.js
@@ -1,6 +1,4 @@
-
-var show = function (item , list, cnt) {
-  
+function show (item, list, cnt) {
   item.addEventListener("click", function () {
     cnt++;
     if (cnt % 2 != 0) {
@@ -11,14 +9,29 @@ var show = function (item , list, cnt) {
   });
 };
 
-
-var exit = function(item, list){
-
-  item.forEach(element => {
-     element.addEventListener("click", function(){
-    list.classList.remove("show");
+function exit (item, list) {
+  item.forEach((element) => {
+    element.addEventListener("click", function () {
+      list.classList.remove("show");
+    });
   });
-  });
+};
 
- 
+function kiemTraDangNhap() {
+  var tenTaiKhoan = document.querySelector("#email").value;
+    var matKhau = document.querySelector("#pass").value;
+  
+  dangNhap(tenTaiKhoan, matKhau);
 }
+
+function  dangNhap ( tenTaiKhoan, matKhau) {
+    if (tenTaiKhoan === "user@gmail.com" && matKhau === "123456") {
+      
+      window.location.href = "https://tkw-tim-kiem-main-rho.vercel.app/";
+    } else if (tenTaiKhoan === "admin@gmail.com" && matKhau === "123456") {
+       window.location.href = "https://tkw-ad-trang-chu.vercel.app/";
+    } else {
+      alert("Sai tên đăng nhập hoặc mật khẩu");
+    }
+  ;
+};
